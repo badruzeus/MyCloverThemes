01d95738e86be8b576981cdfa0c380f91157b85a
Badruzeus Shava <badrus.achmad@gmail.com>
2018-07-06 13:44:55 +0700

    Initial release v1.0

---
+++ MyCloverThemes.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+cd ~/Desktop
+
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/Aero | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/Aero256 | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/Apocalypse | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/Beauty | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/BeautyDark | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/BeautyDark256 | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/BeautyX | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/Buttons | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/Buttons256 | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/Carbon | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/Chamelia | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/Circla | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/Circla256 | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/Classical | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/ClassicalDark | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/Clovernity | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/Crystal | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/DarkBoot | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/DarkBoot256 | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/DarkBootX | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/FlatCamp | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/Glassy | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/Glassy256 | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/Gothic | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/Greenify | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/Greenify256 | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/Hackintosh_ID | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/Hex256 | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/HexaCore | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/HexaCore256 | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/Hexagon | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/HexagonDark | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/HexagonDark256 | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/HighSierra | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/InsanelyLook | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/iOSX | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/iOSX256 | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/Leather | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/Lightness | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/Lightness256 | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/Luxury | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/Luxury2.0 | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/Luxury256 | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/Material | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/Material256 | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/MaterialBox | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/MaterialDark | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/Neon | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/Neon256 | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/Nightwish | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/Nightwish256 | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/One | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/Outlines | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/Outlines256 | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/QB | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/QBDark256 | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/Romance | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/SilverLight | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/Smooth | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/SmoothDark | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/SmoothDark256 | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/Sphere | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/Sphere256 | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/Woody | tar -x -v
+git archive --remote=git://git.code.sf.net/p/cloverefiboot/themes HEAD themes/Xmas | tar -x -v
+
+exit 0
